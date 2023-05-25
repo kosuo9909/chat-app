48dfc0e735e17f2fb40b87d5dc5b5da2af147ce2
Kostadin Stanev <stanebvns@gmail.com>
2023-05-25 21:58:29 +0300

    edited svg icons

--- src/components/Account/Register.jsx
+++ src/components/Account/Register.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import './Register.scss';
+import './Account.scss';
 import auth from '../API/firebase';
 import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
@@ -82,14 +82,17 @@ const Register = () => {
         <div className='login-icon'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            fill='currentColor'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
             className='login-svg'
-            viewBox='0 0 16 16'
           >
-            <path d='M8 5a1 1 0 0 1 1 1v1H7V6a1 1 0 0 1 1-1zm2 2.076V6a2 2 0 1 0-4 0v1.076c-.54.166-1 .597-1 1.224v2.4c0 .816.781 1.3 1.5 1.3h3c.719 0 1.5-.484 1.5-1.3V8.3c0-.627-.46-1.058-1-1.224zM6.105 8.125A.637.637 0 0 1 6.5 8h3a.64.64 0 0 1 .395.125c.085.068.105.133.105.175v2.4c0 .042-.02.107-.105.175A.637.637 0 0 1 9.5 11h-3a.637.637 0 0 1-.395-.125C6.02 10.807 6 10.742 6 10.7V8.3c0-.042.02-.107.105-.175z' />
-            <path d='M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z' />
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z'
+            />
           </svg>
         </div>
         <form onSubmit={handleSubmit} className='form'>
@@ -121,7 +124,7 @@ const Register = () => {
           <button className='button'>Register</button>
           <span>
             You already have an account?{' '}
-            <Link to='/register' className='link'>
+            <Link to='/login' className='link'>
               {' '}
               Log in.
             </Link>
